b73471952de43f5c8c42391c33b65204ef1f1691
Tyler Nickerson <nickersoft@gmail.com>
2023-05-29 18:37:24 -0700

    feat(*): move back to static site

--- src/pages/docs/index.ts
+++
@@ -1,7 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const prerender = false;
-
-export const get: APIRoute = async ({ redirect }) => {
-  return redirect("/docs/getting-started", 307);
-};
