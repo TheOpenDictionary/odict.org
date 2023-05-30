7081d29c20b76eca2b41ed9038f37f4b377daeab
Tyler Nickerson <nickersoft@gmail.com>
2023-05-29 18:29:52 -0700

    feat(*): use Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
+  output: "hybrid",
+  adapter: vercel(),
   experimental: {
     hybridOutput: true,
   },
