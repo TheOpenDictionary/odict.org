b849b8aba967dda598deba43e2bdfb95fe2596e2
Tyler Nickerson <nickersoft@gmail.com>
2023-05-29 18:32:09 -0700

    Revert "feat(*): use Vercel adapter"
    This reverts commit 7081d29c20b76eca2b41ed9038f37f4b377daeab.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
-  adapter: vercel(),
+  output: 'hybrid',
   experimental: {
     hybridOutput: true,
   },
