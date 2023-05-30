8c092ed545a9359a4742609eac1eed565fe51750
Tyler Nickerson <nickersoft@gmail.com>
2023-05-30 16:43:01 -0700

    started quickstart

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  markdown: {
+		rehypePlugins: [
+			rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior:'wrap'
+        }
+      ]
+		]
+	}
 });
