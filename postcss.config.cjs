3366e2d7d43ab01d6c360bb61e0db4cfd2051aed
Tyler Nickerson <nickersoft@gmail.com>
2023-06-26 17:16:57 +0200

    chore(*): update dependencies

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,7 +4,7 @@ const nesting = require('tailwindcss/nesting');
 
 const config = {
 	plugins: [
-		nesting(),
+		nesting,
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
